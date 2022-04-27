a4219096bcac2a76413e27361561fb8e2cd1279e
MelinaAveMol <mmolinapmi@gmail.com>
2022-04-27 15:57:39 +0200

    conociendo js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let picks = document.getElementById("picks");
+
+picks.addEventListener("click",()=>{
+    picks.classList.add("active");
+});
+
+let hamburger=document.getElementById("hamburger",()=>{
+    hamburger.classList.add("active");
+});
+
+
