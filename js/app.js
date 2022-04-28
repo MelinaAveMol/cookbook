7cada7ddc9ae3dcc8aa124476eef397bd0b4025c
MelinaAveMol <mmolinapmi@gmail.com>
2022-04-28 15:18:21 +0200

    1er condicional

--- js/app.js
+++ js/app.js
@@ -1,11 +1,47 @@
+// Hamburguesa
+
+let hamburger = document.getElementById("hamburger");
 let picks = document.getElementById("picks");
 
-picks.addEventListener("click",()=>{
-    picks.classList.add("active");
+if(hamburger){
+    hamburger.addEventListener("click", () => {
+        picks.classList.toggle("active");
+        hamburger.classList.toggle("active");
+    });
+}
+
+
+// Botón más-menos
+
+let quantityPlus = document.getElementById("quantity-plus"); 
+let quantityMinus = document.getElementById("quantity-minus"); 
+let quantityShow = document.getElementById("quantity-show");
+
+
+    quantityPlus.addEventListener("click", () => {
+          
+    
+            if(quantityShow.value < 3){
+                quantityShow.value = (parseInt(quantityShow.value) + 1);
+               
+            }else if (quantityShow.value >= 3){
+               
+                alert("No puedes elegir más de tres talleres simultáneamente");
+            }
+    });
+
+    quantityMinus.addEventListener("click", () => {
+         
+            
+            if(quantityShow.value >1 ){
+                quantityShow.value = (parseInt(quantityShow.value) - 1);
+                
+                
+            }
 });
 
-let hamburger=document.getElementById("hamburger",()=>{
-    hamburger.classList.add("active");
-});
+
+
+
 
 
