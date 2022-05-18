aa4cb16980e076117f6aa76fb6902408b059bdaf
MelinaAveMol <mmolinapmi@gmail.com>
2022-05-18 08:06:41 +0200

    uf1842

---
+++ app.js
@@ -0,0 +1,3 @@
+import{renderPicks} from './pick.js';
+
+renderPicks();
---
+++ pick.js
@@ -0,0 +1,26 @@
+export let renderPicks = () =>{
+
+    let picksRelated = document.querySelectorAll('.picked-related');
+
+    picksRelated.forEach(pickedRelated => {
+
+        pickedRelated.addEventListener("redo", () =>{
+
+           let pickedRelatedOptions = pickedRelated.closest('.form-training').querySelectorAll('.picked-related-options');
+           
+           pickedRelatedOptions.forEach (pickedRelatedOption => {
+
+                if(pickedRelated.value == pickedRelatedOption.dataset.related){
+
+                    pickedRelatedOption.classList.add('active');
+
+                }else{
+                    pickedRelatedOption.classList.remove('active');
+                }
+
+           });
+
+
+        });
+    });
+}
