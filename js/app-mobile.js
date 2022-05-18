3dc1348d1bf2ee4bb5f668ce43dd6ecea02a3578
MelinaAveMol <mmolinapmi@gmail.com>
2022-05-18 08:10:14 +0200

    faltan archivos en local

---
+++ js/app-mobile.js
@@ -0,0 +1,7 @@
+import { renderHamburger } from "./mobile/renderHamburger.js";
+import {renderTabs} from "./mobile/renderTabs.js";
+
+
+
+
+renderHamburger();
---
+++ js/desktop/rendertabs.js
@@ -0,0 +1,25 @@
+// export let renderTabs = () => {
+
+//     let tabs = document.querySelectorAll (".tab");
+//     let relatedTab = document.queryselectorAll (".tab-related");
+
+//     if(tabs) {
+
+//             tabs.forEach((tab, i) => {
+                
+//                 tab.addEventListener("click",  () => {
+//                     tabs.forEach ((tab, i) => {
+//                         relatedtab [i].classlist.remove("active");
+//                         tab.classlist.remove("active");
+
+//                     });
+
+//                     relatedTab[i].classList.add("acive");
+//                     tab.classList.add("active");
+
+//                 });
+//             });
+
+//     }
+
+// }
